cfa5ca4554cab035e618526798f5e161da967fce
ineshdeez <122534302+ineshdeez@users.noreply.github.com>
2023-02-21 13:49:54 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+let x = 0
+let y = 0
+let luz_x = 0
+basic.forever(function () {
+    x = input.acceleration(Dimension.X)
+    y = input.acceleration(Dimension.Y)
+    led.plot(luz_x, 0)
+    led.unplot(0, 0)
+    if (x < 700) {
+        luz_x = 0
+    } else if (x < -200) {
+    	
+    }
+})
